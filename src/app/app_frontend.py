166d81ed0d544183bf460eaaef0727ad25d0fdb5
dominik-pichler <info@dominik-pichler.com>
2025-01-04 19:08:09 +0100

    up

--- src/app/app_frontend.py
+++ src/app/app_frontend.py
@@ -1,8 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 # Set page configuration
 st.set_page_config(
-    page_title="Enterprise ML Platform",
+    page_title="Money Laundering Detector",
     page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded"
@@ -11,6 +11,12 @@ st.set_page_config(
 # Custom CSS for styling
 custom_css = """
 <style>
+
+div.block-container {
+    max-width: 1200px; /* Adjust this value to control the width */
+    padding-left: 2rem;
+    padding-right: 2rem;
+}
 /* Full-width layout adjustments */
 .css-1d391kg { padding: 0 !important; }
 .css-18e3th9 { padding: 0 !important; }
@@ -62,33 +68,69 @@ with st.sidebar:
     st.markdown("[Contact](#contact)", unsafe_allow_html=True)
 
 # Hero Section
-st.markdown("<h1 style='text-align:center;'>Money Laundering Detector 💸</h1>", unsafe_allow_html=True)
-st.markdown(
-    "<p style='text-align:center; font-size:18px;'> Easily indentify money laundering in large networks of finanical transactions .</p>",
-    unsafe_allow_html=True,
-)
+st.markdown("<h1 style='text-align:center;'>Money Laundering Detector </h1>", unsafe_allow_html=True)
+st.markdown("<br>",unsafe_allow_html=True)
+st.markdown("<br>",unsafe_allow_html=True)
 
 # Main Interface Section
 st.markdown("### About")
-st.write("This application is based on modern graph neural networks and geometry-based proprocessing amining to efficiently detect smurf-based-money laundering in large scale transaction networks")
+st.markdown("This application aims at identifying money laundering transactions in arbitray large networks of banking transactions. "
+            "<br>It is based on modern graph neural networks and geometry-based proprocessing and in it's current form, includes trained models with the following architectures", unsafe_allow_html=True)
+
+st.markdown("<br>",unsafe_allow_html=True)
+
+st.markdown("**GATe** (Graph Attention Network with edge features) / GAT <br>"
+            "**GINe** (Graph Isomorphism Network with edge features) <br>"
+            "**PNA** (Principal Neighbourhood Aggregation)<br>"
+            "**RGCN** (Relational Graph Convolutional Network)",unsafe_allow_html=True)
+
+st.markdown("<br>",unsafe_allow_html=True)
 
 
 st.markdown("#### Select Model Version")
 model_version = st.selectbox(
     "Choose a model version:",
-    ["Enterprise Small", "Enterprise Medium", "Enterprise Large"],
+    ["Graph Attention Network with edge features", "Graph Isomorphism Network with edge features", "Principal Neighbourhood Aggregation","Relational Graph Convolutional Network"],
     index=1,
     key="model_version",
     help="Select the version of the model you want to use."
 )
+st.markdown("<br>",unsafe_allow_html=True)
+
+
+# Streamlit app layout
 st.markdown("#### Upload Data")
+
+st.markdown("""
+Please consider that the application can only work with data in the following structure:
+""")
+
+# Display the DataFrame in an interactive table
+st.markdown("""
+| Timestamp       | From Bank | Account    | To Bank | Account    | Amount Received | Receiving Currency | Amount Paid | Payment Currency | Payment Format |
+|-----------------|-----------|------------|---------|------------|-----------------|--------------------|-------------|------------------|----------------|
+| 01.09.22 0:08   | 11        | 8000ECA90  | 11      | 8000ECA90  | 3195403         | US Dollar          | 3195403     | US Dollar        | Reinvestment   |
+| 01.09.22 0:21   | 3402      | 80021DAD0  | 3402    | 80021DAD0  | 1858.96         | US Dollar          | 1858.96     | US Dollar        | Reinvestment   |
+| 01.09.22 0:00   | 11        | 8000ECA90  | 1120    | 8006AA910  | 592571          | US Dollar          | 592571      | US Dollar        | Cheque         |
+| 01.09.22 0:16   | 3814      | 8006AD080  | 3814    | 8006AD080  | 12.32           | US Dollar          | 12.32       | US Dollar        | Reinvestment   |
+| 01.09.22 0:00   | 20        | 8006AD530  | 20      | 8006AD530  | 2941.56         | US Dollar          | 2941.56     | US Dollar        | Reinvestment   |
+| 01.09.22 0:24   | 12        | 8006ADD30  | 12      | 8006ADD30  | 6473.62         | US Dollar          | 6473.62     | US Dollar        | Reinvestment   |
+| 01.09.22 0:17   | 11        | 800059120  | 1217    | 8006AD4E0  | 60562           | US Dollar          | 60562       | US Dollar        | ACH            |
+""")
+st.markdown("<br>",unsafe_allow_html=True)
+
+
 uploaded_file = st.file_uploader(
     "Drag and drop or browse to upload your file",
-    type=["csv", "xlsx", "json"],
+    type=["csv"],
     key="file_upload",
     help="Upload your dataset for inference.",
     label_visibility="collapsed"
 )
+
+
+st.markdown("<br>",unsafe_allow_html=True)
+st.markdown("<br>",unsafe_allow_html=True)
 
 st.markdown("### Results Dashboard")
 
@@ -146,6 +188,6 @@ With a focus on reliability, security, and performance, we provide a robust solu
 # Footer Section
 st.markdown("<hr>", unsafe_allow_html=True)
 st.markdown(
-    "<p style='text-align:center;'>&copy; 2024 Enterprise ML Platform. All rights reserved.</p>",
+    "<p style='text-align:center;'>&copy;  Dominik Pichler (info[at]dominik-pichler[dot]com - All rights reserved.</p>",
     unsafe_allow_html=True,
 )
